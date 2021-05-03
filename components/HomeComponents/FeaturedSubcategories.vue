dc6c0c7fa877857b4004f4b868886a416e0793b9
Mike Oxhuge <mikelitoris34@icloud.com>
2021-05-03 11:59:07 +0200

    commit on master

--- components/HomeComponents/FeaturedSubcategories.vue
+++ components/HomeComponents/FeaturedSubcategories.vue
@@ -1,7 +1,7 @@
 <template>
   <v-main class="pt-2">
     <!-- small screens -->
-    <v-slide-group
+    <!-- <v-slide-group
       v-if="$vuetify.breakpoint.mdAndDown"
       :show-arrows="showArrows"
     >
@@ -22,9 +22,9 @@
           </div>
         </NuxtLink>
       </v-slide-item>
-    </v-slide-group>
+    </v-slide-group> -->
 
-    <v-slide-group
+    <!-- <v-slide-group
       v-if="$vuetify.breakpoint.mdAndDown"
       :show-arrows="showArrows"
     >
@@ -45,10 +45,10 @@
           </div>
         </NuxtLink>
       </v-slide-item>
-    </v-slide-group>
+    </v-slide-group> -->
 
     <!-- large screens -->
-    <v-row v-if="$vuetify.breakpoint.lgAndUp">
+    <!-- <v-row v-if="$vuetify.breakpoint.lgAndUp">
       <v-col
         v-for="category in featuredSubCategories"
         :key="category.i"
@@ -70,7 +70,87 @@
           </div>
         </NuxtLink>
       </v-col>
-    </v-row>
+    </v-row> -->
+    <v-carousel hide-delimiters :show-arrows="showArrows">
+      <v-carousel-item>
+        <v-sheet height="100%" tile>
+          <v-row class="fill-height" align="center" justify="center">
+            <v-col v-for="category in firstHalf" :key="category.i" cols="auto">
+              <NuxtLink :to="category.link">
+                <div
+                  class="d-flex flex-column align-center mx-5"
+                  :style="customWidth"
+                >
+                  <v-avatar :size="customSize">
+                    <v-img
+                      :src="category.img"
+                      alt="featured sub category image"
+                    />
+                  </v-avatar>
+                  <v-btn text>
+                    <span class="fire--text">
+                      {{ category.name }}
+                    </span>
+                  </v-btn>
+                </div>
+              </NuxtLink>
+            </v-col>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+
+      <v-carousel-item>
+        <v-sheet height="100%" tile>
+          <v-row class="fill-height" align="center" justify="center">
+            <v-col v-for="category in secondHalf" :key="category.i" cols="auto">
+              <NuxtLink :to="category.link">
+                <div
+                  class="d-flex flex-column align-center mx-5"
+                  :style="customWidth"
+                >
+                  <v-avatar :size="customSize">
+                    <v-img
+                      :src="category.img"
+                      alt="featured sub category image"
+                    />
+                  </v-avatar>
+                  <v-btn text class="fire--text">
+                    {{ category.name }}
+                  </v-btn>
+                </div>
+              </NuxtLink>
+            </v-col>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+
+      <v-carousel-item>
+        <v-sheet height="100%" tile>
+          <v-row class="fill-height" align="center" justify="center">
+            <v-col v-for="category in thirdHalf" :key="category.i" cols="auto">
+              <NuxtLink :to="category.link">
+                <div
+                  class="d-flex flex-column align-center mx-5"
+                  :style="customWidth"
+                >
+                  <v-avatar :size="customSize">
+                    <v-img
+                      :src="category.img"
+                      alt="featured sub category image"
+                    />
+                  </v-avatar>
+                  <v-btn text>
+                    <span class="fire--text">
+                      {{ category.name }}
+                    </span>
+                  </v-btn>
+                </div>
+              </NuxtLink>
+            </v-col>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
   </v-main>
 </template>
 
@@ -86,27 +166,38 @@ export default {
     return {
       firstHalf: [],
       secondHalf: [],
+      thirdHalf: [],
     }
   },
 
   computed: {
     customWidth() {
-      return this.$vuetify.breakpoint.smAndDown ? 'width: 40vw' : 'width: 10vw'
+      return this.$vuetify.breakpoint.smAndDown ? 'width: 15vw;' : 'width: 10vw'
     },
     showArrows() {
-      return this.$vuetify.breakpoint.smAndDown ? 'false' : ''
+      return !this.$vuetify.breakpoint.smAndDown
+    },
+    customSize() {
+      return this.$vuetify.breakpoint.smAndDown ? '60' : '150'
     },
     //
   },
+
   beforeMount() {
-    const n = this.featuredSubCategories.length / 2 // getting half of the array
-    const a = this.featuredSubCategories.slice() // important to clone it with slice()
-    const b = a.splice(0, n) // splicing right in the middle
+    const x = this.featuredSubCategories.length / 3
+    const a = this.featuredSubCategories.slice()
+    const b = a.splice(0, x)
+    const n = a.length / 2
+    const c = a.splice(0, n)
+
     for (let i = 0; i < a.length; i++) {
       this.firstHalf.push(a[i])
     }
     for (let i = 0; i < b.length; i++) {
       this.secondHalf.push(b[i])
+    }
+    for (let i = 0; i < c.length; i++) {
+      this.thirdHalf.push(c[i])
     }
   },
   mounted() {
