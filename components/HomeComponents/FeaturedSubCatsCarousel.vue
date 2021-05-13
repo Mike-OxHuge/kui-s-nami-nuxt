cc0e015501262a3bcafdc3873a979afd05ac5fd7
Mike Oxhuge <mikelitoris34@icloud.com>
2021-05-13 21:04:59 +0200

    commit on master

---
+++ components/HomeComponents/FeaturedSubCatsCarousel.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-row class="fill-height" align="center" justify="center">
+    <v-col v-for="category in splittedArray" :key="category.i" cols="auto">
+      <div class="d-flex flex-column align-center">
+        <v-avatar :size="customSize">
+          <v-img :src="category.img" alt="featured sub category image" />
+        </v-avatar>
+        <p class="fire--text text-center">
+          {{ category.name }}
+        </p>
+      </div>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    splittedArray: {
+      type: Array,
+      required: true,
+    },
+  },
+  computed: {
+    customSize() {
+      return this.$vuetify.breakpoint.smAndDown ? '60' : '200'
+    },
+  },
+}
+</script>
+
+<style></style>
--- components/HomeComponents/FeaturedSubcategories.vue
+++ components/HomeComponents/FeaturedSubcategories.vue
@@ -2,85 +2,26 @@
   <v-main class="pt-4">
     <v-carousel hide-delimiters :show-arrows="showArrows" height="100vh">
       <v-carousel-item>
-        <v-sheet height="" tile>
-          <v-row class="fill-height" align="center" justify="center">
-            <v-col v-for="category in firstHalf" :key="category.i" cols="auto">
-              <NuxtLink :to="category.link">
-                <div
-                  class="d-flex flex-column align-center mx-5"
-                  :style="customWidth"
-                >
-                  <v-avatar :size="customSize">
-                    <v-img
-                      :src="category.img"
-                      alt="featured sub category image"
-                    />
-                  </v-avatar>
-                  <p class="fire--text text-center">
-                    {{ category.name }}
-                  </p>
-                </div>
-              </NuxtLink>
-            </v-col>
-          </v-row>
-        </v-sheet>
+        <carousel-item :splitted-array="firstHalf" />
       </v-carousel-item>
 
       <v-carousel-item>
-        <v-sheet height="100%" tile>
-          <v-row class="fill-height" align="center" justify="center">
-            <v-col v-for="category in secondHalf" :key="category.i" cols="auto">
-              <NuxtLink :to="category.link">
-                <div
-                  class="d-flex flex-column align-center mx-5"
-                  :style="customWidth"
-                >
-                  <v-avatar :size="customSize">
-                    <v-img
-                      :src="category.img"
-                      alt="featured sub category image"
-                    />
-                  </v-avatar>
-                  <p class="fire--text text-center">
-                    {{ category.name }}
-                  </p>
-                </div>
-              </NuxtLink>
-            </v-col>
-          </v-row>
-        </v-sheet>
+        <carousel-item :splitted-array="secondHalf" />
       </v-carousel-item>
 
       <v-carousel-item>
-        <v-sheet height="100%" tile>
-          <v-row class="fill-height" align="center" justify="center">
-            <v-col v-for="category in thirdHalf" :key="category.i" cols="auto">
-              <NuxtLink :to="category.link">
-                <div
-                  class="d-flex flex-column align-center mx-5"
-                  :style="customWidth"
-                >
-                  <v-avatar :size="customSize">
-                    <v-img
-                      :src="category.img"
-                      alt="featured sub category image"
-                    />
-                  </v-avatar>
-                  <p class="fire--text text-center">
-                    {{ category.name }}
-                  </p>
-                </div>
-              </NuxtLink>
-            </v-col>
-          </v-row>
-        </v-sheet>
+        <carousel-item :splitted-array="thirdHalf" />
       </v-carousel-item>
     </v-carousel>
   </v-main>
 </template>
 
 <script>
+import carouselItem from '~/components/HomeComponents/FeaturedSubCatsCarousel.vue'
 export default {
+  components: {
+    carouselItem,
+  },
   props: {
     featuredSubCategories: {
       type: Array,
@@ -96,17 +37,8 @@ export default {
   },
 
   computed: {
-    // this is really bad
-    // customWidth() {
-    //   return this.$vuetify.breakpoint.smAndDown
-    //     ? 'width: 15vw; height:20vh;'
-    //     : 'width: 10vw; height:30vh;'
-    // },
     showArrows() {
       return !this.$vuetify.breakpoint.smAndDown
-    },
-    customSize() {
-      return this.$vuetify.breakpoint.smAndDown ? '60' : '150'
     },
     //
   },
